336ba14ae917977b964a6d89024cb6429997c292
Stefan Maletic <stefan05maletc@gmail.com>
2024-08-26 20:39:22 +0200

    Fixing problems

--- src/App.js
+++ src/App.js
@@ -22,9 +22,11 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <Routes>
+        <Route index element={<Naslovna/>}/>
+      </Routes>
       <div>
         <Routes>
-          <Route index element={<Naslovna />} />
           <Route path="/naslovna" element={<Naslovna />} />
           <Route path="/meni" element={<Meni />} />
           <Route path="/dorucak" element={<Dorucak />} />
